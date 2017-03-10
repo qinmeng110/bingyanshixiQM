61a88d8836d91044d8b47c34c05f73ec0a3328a8
qinmeng110 <2538943627@qq.com>
2017-03-10 23:08:45 +0800

    3.10

---
+++ fifth/js/gaijin.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pc on 2017/3/8.
+ */
+function show(){
+    var menu = document.getElementById("face-menu");
+    menu.style.display="block";
+}
+function hidde(){
+    var menu = document.getElementById("face-menu");
+    menu.style.display="none";
+}
+function chuxian(){
+    var menu = document.getElementsByClassName("f-cb")[0];
+    menu.style.display="block";
+}
+function xiaoshi(){
+    var menu = document.getElementsByClassName("f-cb")[0];
+    menu.style.display="none";
+}
